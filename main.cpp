6006a38d266632b769c672220877dcf3f3b7ecca
7h3730B <teoschulze5@gmail.com>
2020-05-17 21:32:08 +0200

    Going to sleep for now lmao

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
 
 using namespace std;
 
-int main() {
+int main()
+{
+
+    INPUT input;
+    string word;
+    ifstream scriptFile("script.txt");
+
+    if (scriptFile.is_open())
+    {
+        while (scriptFile >> word)
+        {
+            if (!word.empty())
+            {
+                // TODO. Check for keyboard press and send Keyboard press
+                cout << word << '\n';
+            }
+        }
+        scriptFile.close();
+    }
+    else
+    {
+        cout << "Something failed";
+    }
+
+    cout << endl;
+
     return 0;
 }
